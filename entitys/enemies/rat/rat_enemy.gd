f19cf52fcca69705f29865cd1ba2d494fb5b4380
Maru <vlasenko.maria39@gmail.com>
2026-05-25 01:23:12 +0200

    Added WIP version of combat system

--- entitys/enemies/rat/rat_enemy.gd
+++ entitys/enemies/rat/rat_enemy.gd
@@ -1,6 +1,30 @@
 extends CharacterBody2D
+enum {
+	IDLE,
+	ATTACK,
+	CHASE, 
+	DEATH,
+	DAMAGE 
+}
 
-var chase = false
+var state: int = IDLE:
+	set(val):
+		if state == DEATH:
+			return
+		state = val
+		enter_state()
+
+
+var health: int = 20:
+	set(val):
+		if state == DEATH:
+			return
+		health = val
+		if health > 0:
+			state = DAMAGE
+
+var player_pos: Vector2 = Vector2.ZERO 
+var direction: Vector2 = Vector2.ZERO
 var speed = 200.0
 const JUMP_VELOCITY = -400.0 
 
@@ -8,62 +32,103 @@ const JUMP_VELOCITY = -400.0
 @onready var animPlayer = $AnimationPlayer
 var alive = true
 
-func _physics_process(delta: float) -> void:
-	if not alive:
-		return
+func _ready() -> void:
+	Signals.connect("player_position_update", Callable(self,"_on_player_position_update"))
+	animPlayer.animation_finished.connect(_on_animation_finished)
 
+func _on_player_position_update(pos: Vector2):
+	player_pos = pos
+
+func enter_state():
+	match state:
+		IDLE:
+			velocity.x = 0
+			animPlayer.play("Idle")
+			$AttackDirection/AttackRange/CollisionShape2D.set_deferred("disabled", false)
+			$Detector/CollisionShape2D.set_deferred("disabled", false)
+		ATTACK:
+			velocity.x = 0
+			animPlayer.play("Bite")
+			deal_damage()
+		DAMAGE:
+			velocity.x = 0
+			animPlayer.play("GetHit")
+		DEATH:
+			death()
+
+func _physics_process(delta: float) -> void:
+	if health <= 0 and state != DEATH:
+		state = DEATH
+		
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+
+	if state == DEATH:
+		move_and_slide()
+		return
+
+	if state == CHASE:
+		chase_state()
 		
-	var player = $"../../Player/Player"
-	var direction = (player.position - self.position).normalized()
-	
-	if chase == true:
-		velocity.x = direction.x * speed
-		
-		# крыса прыгает, если стоит на земле И (уперлась в стену ИЛИ игрок находится значительно выше)
-		if is_on_floor():
-			if is_on_wall() or (player.position.y < self.position.y - 50):
-				velocity.y = JUMP_VELOCITY	
-		if is_on_floor():
-			anim.play("Run")
-		else:
-			pass 
-	else:
-		velocity.x = move_toward(velocity.x, 0, speed) # Плавная остановка
-		if is_on_floor():
-			anim.play("Idle")
-		
-	# Разворот спрайта
-	if direction.x < 0 :
-		anim.flip_h = true
-	elif direction.x > 0 :
-		anim.flip_h = false
-	
 	move_and_slide()
 
+
+func chase_state():
+	direction = (player_pos - self.position).normalized()
+	
+	velocity.x = direction.x * speed
+	
+	if direction.x < 0 :
+		anim.flip_h = true
+		$AttackDirection.rotation_degrees = 180
+	elif direction.x > 0 :
+		anim.flip_h = false
+		$AttackDirection.rotation_degrees = 0
+		
+	if is_on_floor():
+		if is_on_wall() or (player_pos.y < self.position.y - 50):
+			velocity.y = JUMP_VELOCITY
+		animPlayer.play("Run")
+
+func deal_damage():
+	var bodies = $AttackDirection/AttackRange.get_overlapping_bodies()
+	for body in bodies:
+		if body.name == "Player":
+			body.health -= 20
+
+
+
+func _on_animation_finished(anim_name: String):
+	if anim_name == "Bite" and state != DEATH:
+		$AttackDirection/AttackRange/CollisionShape2D.set_deferred("disabled", true)
+		$Detector/CollisionShape2D.set_deferred("disabled", true)
+		state = IDLE
+	
+	# 4. Выход из состояния получения урона
+	elif anim_name == "GetHit" and state != DEATH:
+		# После получения удара крыса агрессивно продолжает погоню
+		state = CHASE
+
 func _on_detector_body_entered(body: Node2D) -> void:
-	if body.name == "Player":
-		chase = true
+	if body.name == "Player" and state != DEATH:
+		state = CHASE
 
 func _on_detector_body_exited(body: Node2D) -> void:
-	if body.name == "Player":
-		chase = false
+	if body.name == "Player" and state != DEATH:
+		state = IDLE
+
+func _on_attack_range_body_entered(body: Node2D) -> void:
+	if body.name == "Player" and state != DEATH:
+		state = ATTACK
 
 func _on_detector_death_body_entered(body: Node2D) -> void:
-	if body.name == "Player" and alive:
+	if body.name == "Player" and state != DEATH:
 		body.velocity.y -= 200 # Отскок игрока
-		death()
-
-func _on_detector_attack_body_entered(body: Node2D) -> void:
-	if body.name == "Player" and alive:
-		animPlayer.play("Bite")
-		body.health -= 100
-		death()
+		state = DEATH
 
 func death():
 	alive = false
-	velocity = Vector2.ZERO 
-	$CollisionShape2D.set_deferred("disabled", true) 
+	velocity.x = 0
+	$AttackDirection/AttackRange/CollisionShape2D.set_deferred("disabled", true)
 	anim.play("Death")
 	anim.animation_finished.connect(queue_free)
--- player.gd
+++ player.gd
@@ -6,6 +6,7 @@ enum {
 	FALL,
 	WALLSLIDE,
 	ATTACK,
+	DAMAGE,
 	DEATH 
 }
 
@@ -13,7 +14,8 @@ enum {
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 const WALL_SLIDE_SPEED = 120.0  
-const WALL_JUMP_PUSH = 200.0   
+const WALL_JUMP_PUSH = 200.0  
+const player_damage = 10 
 
 @onready var anim = $"AnimatedSprite2D"
 @onready var animPlay = $AnimationPlayer
@@ -21,8 +23,19 @@ const WALL_JUMP_PUSH = 200.0
 @onready var wallJumpLockTimer = $WallJumpLock
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var health = 100
+var health: int = 100:
+	set(val):
+		if state == DEATH:
+			return
+		if val < health:
+			state = DAMAGE
+			animPlay.play("Damage")
+		health = val
+		
 var crystal = 0
+var bodyInAttackRange = false
+var target
+var player_pos 
 var state = MOVE
 
 func _ready() -> void:
@@ -44,13 +57,19 @@ func _physics_process(delta: float) -> void:
 		JUMP:
 			jump_state(delta)
 		FALL:
+			$CollisionShape2D.shape.size = Vector2(10, 14)
 			fall_state(delta)
 		WALLSLIDE:
+			$CollisionShape2D.shape.size = Vector2(10, 14)
 			wallslide_state(delta)
 		ATTACK:
 			attack_state(delta)
+		DAMAGE:
+			damage_state(delta) 
 
 	move_and_slide()
+	player_pos = self.position
+	Signals.emit_signal("player_position_update", player_pos)
 	if was_on_floor and not is_on_floor() and velocity.y >= 0:
 		coyoteTimer.start()
 
@@ -60,8 +79,10 @@ func _physics_process(delta: float) -> void:
 func move_state(delta: float) -> void:
 	apply_base_movement(delta)
 	if velocity.x != 0:
+		$CollisionShape2D.shape.size = Vector2(19, 14)
 		animPlay.play("Run")
 	else:
+		$CollisionShape2D.shape.size = Vector2(19, 14)
 		animPlay.play("Idle")
 		
 	if not is_on_floor():
@@ -98,6 +119,7 @@ func fall_state(delta: float) -> void:
 
 func wallslide_state(delta: float) -> void:
 	velocity.y = move_toward(velocity.y, WALL_SLIDE_SPEED, gravity * delta)
+	$CollisionShape2D.shape.size = Vector2(11, 14)
 	animPlay.play("WallSlide")
 	
 	var direction := Input.get_axis("left", "right")
@@ -111,7 +133,11 @@ func wallslide_state(delta: float) -> void:
 
 func attack_state(delta: float) -> void:
 	apply_base_movement(delta)
-
+	
+func damage_state(delta: float) -> void:
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	velocity.x = move_toward(velocity.x, 0, SPEED)
 
 #вспомогательные функции
 func apply_base_movement(delta: float) -> void:
@@ -124,6 +150,10 @@ func apply_base_movement(delta: float) -> void:
 			velocity.x = direction * SPEED
 			if not is_on_wall() or is_on_floor():
 				anim.flip_h = (direction == -1)
+				if (direction == -1):
+					$AttackDirection.rotation_degrees = 180
+				else:
+					$AttackDirection.rotation_degrees = 0
 		else:
 			velocity.x = move_toward(velocity.x, 0, SPEED)
 
@@ -137,11 +167,22 @@ func perform_wall_jump() -> void:
 	velocity.x = wall_normal.x * WALL_JUMP_PUSH
 	wallJumpLockTimer.start()
 	anim.flip_h = wall_normal.x < 0
+	if (wall_normal.x < 0):
+		$AttackDirection.rotation_degrees = 180
+	else:
+		$AttackDirection.rotation_degrees = 0
 	state = JUMP
 
 func perform_attack() -> void:
 	animPlay.play("Attack")
 	state = ATTACK
+	if bodyInAttackRange and target != null:
+		target.health -= player_damage
+		if ($AttackDirection.rotation_degrees ==0):
+			target.velocity.x += 200
+		else:
+			target.velocity.x -= 200
+		
 
 func handle_death() -> void:
 	velocity.x = 0
@@ -151,8 +192,17 @@ func handle_death() -> void:
 	
 
 func _on_animation_finished(anim_name: String) -> void:
-	if anim_name == "Attack":
+	if anim_name == "Attack" or anim_name == "Damage":
 		if is_on_floor():
 			state = MOVE
 		else:
 			state = FALL
+
+
+func _on_attack_range_body_entered(body: Node2D) -> void:
+	bodyInAttackRange = true
+	target = body
+
+
+func _on_attack_range_body_exited(body: Node2D) -> void:
+	bodyInAttackRange = false
